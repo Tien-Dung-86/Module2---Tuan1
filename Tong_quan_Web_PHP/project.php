7337ca06ba43df2b4e8f96b284f8392a0e9ec2e1
Tien-Dung-86 <tiendungtabd.11@gmail.com>
2021-08-06 16:01:21 +0700

    up bai Danh sach khach hang

--- Tong_quan_Web_PHP/project.php
+++ Tong_quan_Web_PHP/project.php
@@ -1,14 +1,62 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport"
-              content="width=device-width', user-scalable=no, initial-scale=1, maximum-scale=1.0, minium-scale=1">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Ứng dụng hiển thị thời gian của server</title>
-    </head>
-<body>
-<h1>Bây giờ là: <?php echo date('Y-M-d h:i:s'); ?></h1>
-</body>
-</html>
+<?php
+//$txt = "W3School.com";
+//echo "I love $txt!" . "<br>";
+//echo "I love ". $txt . "!";
 
+//$x = 5;
+//$y = 6;
+//$z = $x + $y;
+//echo $z;
+
+//$x = 5;
+//function myTest($y){
+//    $x = 10;
+//    echo "Variable x inside function is: $x";
+//}
+//
+//myTest(20);
+////echo "Variable x inside function is: $x";
+///
+///
+
+$x = 10;
+$y = 10;
+
+//function myTest(){
+////    global $x, $y;
+////    $y += $x;
+//    $GLOBALS['y'] += $GLOBALS['x'];
+//}
+//
+//myTest();
+//echo $y;
+
+//function myTest()
+//{
+//    static $x = 0;
+//    echo $x . "<br>";
+//    $x++;
+//}
+//
+//myTest();
+//myTest();
+//myTest();
+
+
+//$x = 10.35;
+//$y = true;
+//$z = array('Volvo', 'BMW', 'Toyota');
+//var_dump($x);
+//var_dump($y);
+//var_dump($z);
+
+class Car{
+    public string $model;
+    function _construct(){
+        $this->model = "VW";
+    }
+}
+
+$herbie = new Car();
+echo $herbie->model;
+?>
