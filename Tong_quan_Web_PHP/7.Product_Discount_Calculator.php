ba3030d812d8dbe464536ec043fbbc8c0e4aca2e
Tien-Dung-86 <tiendungtabd.11@gmail.com>
2021-08-07 17:25:36 +0700

    Up Produc Discount Calculator

--- Tong_quan_Web_PHP/7.Product_Discount_Calculator.php
+++ Tong_quan_Web_PHP/7.Product_Discount_Calculator.php
@@ -6,30 +6,45 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <style>
+        .container{
+            width: 250px;
+            border: 1px solid black;
+            position: absolute;
+        }
+        input{
+            padding: 2px;
+            margin: 5px;
+            position: relative;
+        }
+    </style>
 </head>
 <body>
 <form action="display_discount.php" method="post">
-    <div>
+    <div class="container">
         <h3>Product Discount Calculator</h3>
         <label>
             Product Description:
             <input type="text" name="product" placeholder="product's name">
         </label>
+        <br><br>
         <label>
             List Price:
             <input type="text" name="price" placeholder="price">
         </label>
+        <br><br>
         <label>
             Discount Percent:
             <input type="text" name="discount" placeholder="discount">
         </label>
-        <br>
+        <br><br>
         <input type="submit" value="Calculate">
     </div>
 </form>
 </body>
 </html>
-
 <?php
-if($_SERVER['REQUEST_METHOD'] == 'POST')
+include "Tong_quan_Web_PHP/display_discount.php";
 ?>
+
+
--- Tong_quan_Web_PHP/display_discount.php
+++ Tong_quan_Web_PHP/display_discount.php
@@ -1,1 +1,12 @@
 <?php
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+    $product = $_POST['product'];
+    $price = $_POST['price'];
+    $discount = $_POST['discount'];
+}
+$discountAmount = $price * $discount * 0.1;
+$discountPrice = $price - $discountAmount;
+echo "Product's name: ".$product."<br/>";
+echo "Discount Amount: ".$discountAmount."<br/>";
+echo "The Product's price after disount: ".$discountPrice."<br/>";
+?>
