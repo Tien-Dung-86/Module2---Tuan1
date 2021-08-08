9e8469381601e7e5c7c2e89b82c80f8c8ac35ee1
Tien-Dung-86 <tiendungtabd.11@gmail.com>
2021-08-08 09:33:36 +0700

    Up Prime

---
+++ Tong_quan_Web_PHP/11.Prime.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<?php
+function isPrime($number)
+{
+    for ($i = 2; $i < $number; $i++) {
+        if ($number % $i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+$number = 2;
+$count = 0;
+while ($count < 50) {
+    if ($number < 100) {
+        if (isPrime($number)) {
+            echo $number . '<br>';
+            $count++;
+        }
+        $number++;
+    } else{
+        break;
+    }
+}
+?>
+</body>
+</html>
