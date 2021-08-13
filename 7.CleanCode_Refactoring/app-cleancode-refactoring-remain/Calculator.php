ee0beb78ba36038adae4c8e07b69f58e11780ba2
Tien-Dung-86 <tiendungtabd.11@gmail.com>
2021-08-13 09:18:49 +0700

    Up Clean code and refactoring

---
+++ 7.CleanCode_Refactoring/app-cleancode-refactoring-remain/Calculator.php
@@ -0,0 +1,28 @@
+<?php
+
+const ADDITION = '+';
+const SUBTRACTION = '-';
+const MULTIPLICATION = '*';
+const DIVISION = '/';
+class Calculator
+{
+    public function calculate($firstOperand, $secondOperand, $operator) : float|int
+    {
+        switch ($operator) {
+            case '+':
+                return $firstOperand + $secondOperand;
+            case '-':
+                return $firstOperand - $secondOperand;
+            case '*':
+                return $firstOperand * $secondOperand;
+            case '/':
+                if ($secondOperand != 0) {
+                    return $firstOperand / $secondOperand;
+                } else {
+                    return "Can not divide by 0";
+                }
+            default:
+                return "Unsupported operation";
+        }
+    }
+}
